7b09dd368a13fc8ec7689cb6f661cd1daa4a097f
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-04-22 21:09:19 -0400

    added payload to action to increment by 5

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -5,12 +5,12 @@ import { increment, decrement } from './actions';
 function App() {
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
-  const dipsatch = useDispatch();
+  const dispatch = useDispatch();
   return (
     <div className="App">
      <h1>Counter {counter}</h1>
-     <button>+</button>
-     <button>-</button>
+     <button onClick={() => dispatch(increment(5))}>+</button>
+     <button onClick={() => dispatch(decrement(5))}>-</button>
 
      {isLogged ? <h3>Valuable infor that I shouldnt see if I am not logged in</h3> : ""}
 
