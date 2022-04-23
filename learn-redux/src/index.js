b7666d1f66d19227a705e86403a4c53d8e5bbb85
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-04-22 20:36:32 -0400

    added redux chrom extension

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import { createStore } from 'redux';
 import allReducers from './reducers';
 
-const store = createStore(allReducers);
+const store = createStore(
+  allReducers
+  + window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 
 
