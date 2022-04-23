2f5f680d6667b68b83ee48fc2f0d65d5f2a1f94a
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-04-22 20:59:36 -0400

    added increment and decrement buttons and imported useDispatch to change the state

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,9 +1,19 @@
-
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dipsatch = useDispatch();
   return (
     <div className="App">
-     <h1>Hello World</h1>
+     <h1>Counter {counter}</h1>
+     <button>+</button>
+     <button>-</button>
+
+     {isLogged ? <h3>Valuable infor that I shouldnt see if I am not logged in</h3> : ""}
+
     </div>
   );
 }
--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -7,8 +7,8 @@ import allReducers from './reducers';
 import { Provider } from 'react-redux';
 
 const store = createStore(
-  allReducers
-  + window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
 
