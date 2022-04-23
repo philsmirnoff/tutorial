e02350580d71d68e70f224a036d4174835985b93
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-04-22 20:24:39 -0400

    created the store and added combinedReducer AllReducers to it

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -2,53 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import { createStore } from 'redux';
+import allReducers from './reducers';
+
+const store = createStore(allReducers);
 
 
 
-// STORE -> GLOBAL STATE
+// // STORE -> GLOBAL STATE
 
-// ACTION -> ACTION CREATOR -> DISPATCH -> REDUCER -> STATE
-// ACTION types are strings
+// // ACTION -> ACTION CREATOR -> DISPATCH -> REDUCER -> STATE
+// // ACTION types are strings
 
-// const INCREMENT = 'INCREMENT'
-// const DECREMENT = 'DECREMENT'
+// // const INCREMENT = 'INCREMENT'
+// // const DECREMENT = 'DECREMENT'
 
-// ACTION INCREMENT, action is a function that simply returns an object
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
+// // ACTION INCREMENT, action is a function that simply returns an object
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
 
-  const decrement = () => {
-    return {
-      type: 'DECREMENT'
-    }
-  }
+//   const decrement = () => {
+//     return {
+//       type: 'DECREMENT'
+//     }
+//   }
 
-// REDUCER - describes how your actions transforms the state
-// it takes in the current state and the action and returns the new state
+// // REDUCER - describes how your actions transforms the state
+// // it takes in the current state and the action and returns the new state
 
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-  }
-}
+// const counter = (state = 0, action) => {
+//   switch(action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//   }
+// }
 
-let store = createStore(counter);
+// let store = createStore(counter);
 
-// Display the current state of the store
-store.subscribe(() => console.log(store.getState()));
+// // Display the current state of the store
+// store.subscribe(() => console.log(store.getState()));
 
-// DISPATCH - thats the way we can execute the action , it sends the action to the store
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+// // DISPATCH - thats the way we can execute the action , it sends the action to the store
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 
 
--- learn-redux/src/reducers/isLogged.js
+++ learn-redux/src/reducers/isLogged.js
@@ -2,5 +2,7 @@ const loggedReducer = (state = false, action) => {
   switch(action.type) {
     case 'LOGIN':
       return true;
+    default:
+      return state;
   }
 }
