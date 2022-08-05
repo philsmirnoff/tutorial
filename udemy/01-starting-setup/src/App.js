7a44a1233f6f9b50c5f98f24e8b40e7be7998267
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-08-05 15:29:47 -0400

    add

---
+++ udemy/01-starting-setup/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './components/ExpenseItem';
+import index.css from './index.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h2>Let's get started!</h2>
+      < ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
