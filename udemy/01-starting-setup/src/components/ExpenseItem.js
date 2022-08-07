d957ee05ae92ea5fbeaca013f5d374bafb0cb3a0
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-08-07 14:11:43 -0400

    add dynamic data

--- udemy/01-starting-setup/src/components/ExpenseItem.js
+++ udemy/01-starting-setup/src/components/ExpenseItem.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 0, 1);
+  const expenseTitle = 'Car insurance';
+  const expenseAmount = '$100';
+
   return (
     <div className="expense-item">
-      <div>August'6th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance:</h2>
-        <div className="expense-item_price">$300</div>
+        <h2>{expenseTitle}:</h2>
+        <div className="expense-item_price">${expenseAmount}</div>
       </div>
     </div>
   )
