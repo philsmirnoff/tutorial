429786eda6c10daa57984b537baec65b24bc38d2
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-08-10 17:47:10 -0400

    add ExpenseDate component

--- udemy/01-starting-setup/src/components/ExpenseItem.js
+++ udemy/01-starting-setup/src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2022, 0, 1);
-  // const expenseTitle = 'Car insurance';
-  // const expenseAmount = '$100';
+
 
   return (
     <div className="expense-item">
-      <div>{props.date.toLocaleDateString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}:</h2>
-        <div className="expense-item_price">${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   )
