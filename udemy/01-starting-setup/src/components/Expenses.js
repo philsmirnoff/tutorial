2bfe9be8dbd8770e4ddaade06d04b7904dd17c0f
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-08-10 22:02:32 -0400

    add Expense component

---
+++ udemy/01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Expenses.css'
+import ExpenseItem from './ExpenseItem'
+
+
+function Expenses(props) {
+
+    const { expense } = props;
+    return (
+      <div className="expenses">
+        {expense.map(item => (
+          <ExpenseItem
+          key={item.id}
+          title={item.title}
+          date={item.date}
+          amount={item.amount}
+          />
+        ))}
+      </div>
+    )
+}
+    //  <div className="expenses">
+    // //   <ExpenseItem
+    // //   title={props.expense[0].title}
+    // //   amount={props.expense[0].amount}
+    // //   date={props.expense[0].date}
+    // //    />
+    // //    <ExpenseItem
+    // //   title={props.expense[1].title}
+    // //   amount={props.expense[1].amount}
+    // //   date={props.expense[1].date}
+    // //    />
+    // //    <ExpenseItem
+    // //   title={props.expense[2].title}
+    // //   amount={props.expense[2].amount}
+    // //   date={props.expense[2].date}
+    // //    />
+    // //     <ExpenseItem
+    // //   title={props.expense[3].title}
+    // //   amount={props.expense[3].amount}
+    // //   date={props.expense[3].date}
+    // //    />
+    // // </div>
+    // // );
+// }
+
+export default Expenses
