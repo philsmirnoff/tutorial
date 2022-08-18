a79f25c61fb3a3c0313eea552d83ce0f88fd9d49
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-08-18 02:04:58 -0400

    added ternary operator for the message

--- udemy/01-starting-setup/src/components/Expenses/Expenses.js
+++ udemy/01-starting-setup/src/components/Expenses/Expenses.js
@@ -23,13 +23,17 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
+        {filteredExpenses.length === 0 ? (
+          <h2>No expenses found</h2>
+        ) : (
+        filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           />
+        )
         ))}
       </Card>
     </div>
