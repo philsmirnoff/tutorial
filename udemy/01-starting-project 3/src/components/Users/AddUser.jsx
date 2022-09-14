6afa5f5b6716ce05f5ae061c1c992834b6c75208
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-09-13 22:09:37 -0400

    added ErrorModal

--- udemy/01-starting-project 3/src/components/Users/AddUser.jsx
+++ udemy/01-starting-project 3/src/components/Users/AddUser.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 
 const AddUser = (props) => {
@@ -32,6 +33,8 @@ const AddUser = (props) => {
   }
 
   return (
+    <div>
+    <ErrorModal title="An error occured!" message="Something went wrong!" />
     <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
@@ -48,6 +51,7 @@ const AddUser = (props) => {
       <Button type="submit">Add User</Button>
     </form>
     </Card>
+  </div>
   )
 }
 
