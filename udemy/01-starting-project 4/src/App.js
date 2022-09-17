29d0658d38383e6839f59c23eb6010ceacd0d13f
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-09-17 16:38:38 -0400

    useEffectHooks

--- udemy/01-starting-project 4/src/App.js
+++ udemy/01-starting-project 4/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,26 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+
+  // this function will runs once when the component is rendered for the first time,
+  // and then it will run again whenever the dependencies change and we run it as side effect with useEffect to avoid infinite loops and make sure that code that can be intensive does not run for every component re-render cycle, only if we wanted to run it when the dependencies change
+  useEffect(() => {
+    const storedUserLoggedinInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedinInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('IsLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
@@ -29,3 +42,5 @@ function App() {
 }
 
 export default App;
+
+
