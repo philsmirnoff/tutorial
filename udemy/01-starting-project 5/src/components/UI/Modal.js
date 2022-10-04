ae8a5defc9f2cfff346aa39d83917593babc8aad
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-10-03 21:24:36 -0400

    added Modal

---
+++ udemy/01-starting-project 5/src/components/UI/Modal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import classes from './Modal.module.css';
+import Fragment from 'react';
+import ReactDOM from 'react-dom';
+
+const Backdrop = props => {
+  return <div className={classes.backdrop} onClick={props.onCloseCart} />;
+};
+
+const ModalOverlay = props => {
+  return <div className={classes.modal}>
+    <div className={classes.content}>{props.children}</div>
+  </div>;
+};
+
+const portElement = document.getElementById('overlays');
+
+function Modal(props) {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portElement)}
+      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portElement)}
+    </Fragment>
+  )
+}
+
+export default Modal
