ebe1f4289eee630fdbda9c72559de0308c6fb723
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-10-05 01:56:49 -0400

    adding http error handling

--- udemy/01-starting-project 6/src/App.js
+++ udemy/01-starting-project 6/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
     // fetch rturns us a promise that allows us to react to response
     // we will add then. to define the function that will be called
@@ -23,8 +25,15 @@ function App() {
 
       // new code
       async function fetchMovieHandler() {
+        setIsLoading(true);
+        setError(null);
+        try {
         const response = await fetch('https://swapi.dev/api/films/');
+        if(!response.ok) {
+          throw new Error('Something went wrong!');
+        }
         const data = await response.json();
+
         const transformedMovies = data.results.map(movieData => {
           return {
             id: movieData.episode_id,
@@ -35,8 +44,12 @@ function App() {
         });
         // setMovies(data.results);
         setMovies(transformedMovies);
-      };
 
+      } catch (error) {
+        setError(error.message);
+      };
+      setIsLoading(false);
+    }
 
   // const dummyMovies = [
   //   {
@@ -59,7 +72,13 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {/* // if we are not loading we want to render the movies list */}
+        {!isLoading &&movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+        {/* // if we are loading we want to render the loading message */}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading...</p>}
+
       </section>
     </React.Fragment>
   );
