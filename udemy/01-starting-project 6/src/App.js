7cab5e4b1bfd22541bac7a89e704048fc46102f8
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-10-04 23:41:32 -0400

    added fetch data

---
+++ udemy/01-starting-project 6/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+   function fetchMovieHandler() {
+    // fetch rturns us a promise that allows us to react to response
+    // we will add then. to define the function that will be called
+   // once the data is fetched and we can add catch to handle errors
+     fetch('https://swapi.dev/api/films/')
+      .then(response => {
+        // this response argument here is an object with data about the response and we use json() to transform the data into a js object
+        return response.json();
+      })
+      // once the data treansformation is over we get the data as an argument
+      // and wwe can access to the array through the results property and store it in the movies state so we can update later
+      .then(data => {
+        const transformedMovies = data.results.map(movieData => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date
+          };
+        });
+        // setMovies(data.results);
+        setMovies(transformedMovies);
+      });
+  }
+
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: 'Some Dummy Movie',
+  //     openingText: 'This is the opening text of the movie',
+  //     releaseDate: '2021-05-18',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Some Dummy Movie 2',
+  //     openingText: 'This is the second opening text of the movie',
+  //     releaseDate: '2021-05-19',
+  //   },
+  // ];
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
