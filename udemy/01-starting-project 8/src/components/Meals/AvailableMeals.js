45a12a38c4612ad32a850287bd6fbd083220c5bd
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-10-13 01:47:36 -0400

    added  error  handling

--- udemy/01-starting-project 8/src/components/Meals/AvailableMeals.js
+++ udemy/01-starting-project 8/src/components/Meals/AvailableMeals.js
@@ -33,10 +33,16 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://food-a3685-default-rtdb.firebaseio.com/meals.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -51,13 +57,25 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     }
-   fetchMeals();
+
+     fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+     })
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
