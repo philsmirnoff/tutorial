ddd7ee7da63cd64a21559a4c651235c052a301f8
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-10-27 01:11:49 -0400

    reading and dispatcngfrom a nw slice

--- udemy/01-starting-project 8-9/src/App.js
+++ udemy/01-starting-project 8-9/src/App.js
@@ -1,14 +1,18 @@
 import Counter from './components/Counter';
+import { useSelector, useDispatch } from 'react-redux';
 import { Fragment } from 'react';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
   return (
     <Fragment>
       <Header />
-      <Auth />
+     {!isAuth && <Auth /> }
+     {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
 
