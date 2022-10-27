9fd597f6ecda0f1018963eea40426802f764fd94
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-10-27 00:22:58 -0400

    working with multiple slices

--- udemy/01-starting-project 8-9/src/App.js
+++ udemy/01-starting-project 8-9/src/App.js
@@ -1,9 +1,17 @@
 import Counter from './components/Counter';
+import { Fragment } from 'react';
+import Header from './components/Header';
+import Auth from './components/Auth';
 
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
+
   );
 }
 
