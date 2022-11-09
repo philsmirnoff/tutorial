6b34236c0241d5c5b8430bc7e88cabe81aba0ccf
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-09 01:30:04 -0500

    redirected the user

--- udemy/01-starting-project 8 router/src/pages/ProductDetail.js
+++ udemy/01-starting-project 8 router/src/pages/ProductDetail.js
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom'
 
 function ProductDetail() {
   const params = useParams();
-  console.log(params);
+  console.log(params.productId);
   return (
      <section>
       <h1>Product Detail</h1>
