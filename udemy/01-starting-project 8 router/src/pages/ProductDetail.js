c7e9240802daaabd10dc72a94200d9f28029a0e7
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-09 01:03:33 -0500

    extracting route params

--- udemy/01-starting-project 8 router/src/pages/ProductDetail.js
+++ udemy/01-starting-project 8 router/src/pages/ProductDetail.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
+
 
 function ProductDetail() {
+  const params = useParams();
+  console.log(params);
   return (
-    <section>
+     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   )
 }
