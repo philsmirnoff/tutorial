de2b530a46cfebbb9bb5803a2bc9154953b3eacd
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-09 00:55:05 -0500

    added dyamic routing with :productId

--- udemy/01-starting-project 8 router/src/pages/Products.js
+++ udemy/01-starting-project 8 router/src/pages/Products.js
@@ -3,7 +3,14 @@ import React from 'react';
 
 function Products() {
   return (
-    <h1>Products Page</h1>
+    <>
+       <h1>Products Page</h1>
+    <ul>
+      <li>A Book</li>
+      <li>A Carpet</li>
+      <li>An Online Course</li>
+    </ul>
+    </>
   )
 }
 
