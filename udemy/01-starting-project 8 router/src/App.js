1fe4958a710285be6e78c912370b7981af76c493
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-08 23:46:23 -0500

    replaced anchor tag to <Link>

--- udemy/01-starting-project 8 router/src/App.js
+++ udemy/01-starting-project 8 router/src/App.js
@@ -1,17 +1,23 @@
 import { Route } from 'react-router-dom';
-import Products from './components/Products';
-import Welcome from './components/Welcome';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
       <Route path="/welcome" exact>
         <Welcome />
       </Route>
       <Route path="/products">
         <Products />
       </Route>
-    </div>
+      </main>
+     </div>
   );
 }
 
