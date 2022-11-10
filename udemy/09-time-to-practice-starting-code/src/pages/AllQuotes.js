4e661ba1171d3972c8ee37540078ac0db4a08d23
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-10 01:43:45 -0500

    added quote form

--- udemy/09-time-to-practice-starting-code/src/pages/AllQuotes.js
+++ udemy/09-time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import QuoteList from '../components/quotes/QuoteList';
+
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
 
 const AllQuotes = () => {
   return (
-    <div>AllQuotes</div>
+    <QuoteList quotes={DUMMY_QUOTES} />
   )
 }
 
--- udemy/09-time-to-practice-starting-code/src/pages/NewQuote.js
+++ udemy/09-time-to-practice-starting-code/src/pages/NewQuote.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+  };
   return (
-    <div>NewQuote</div>
+    <QuoteForm onAddQuoteForm={addQuoteHandler} />
   )
 }
 
