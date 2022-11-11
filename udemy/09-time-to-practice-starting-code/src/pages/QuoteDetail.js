64652d379ebb85393d04fcc0c879ee34b69bb52e
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-11 15:33:52 -0500

    outputt the data on details page

--- udemy/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ udemy/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,13 +1,27 @@
 import { Fragment } from 'react';
 import { useParams, Route } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
 
 const QuoteDetail = () => {
   const params = useParams();
 
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <p>{params.quoteId}</p>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
