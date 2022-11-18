ec40b1b8adde4b57784e487a97dde99b86ebf483
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-18 15:52:28 -0500

    added not found page

--- udemy/09-time-to-practice-starting-code/src/App.js
+++ udemy/09-time-to-practice-starting-code/src/App.js
@@ -3,6 +3,7 @@ import AllQuotes from './pages/AllQuotes';
 import NewQuote from './pages/NewQuote';
 import QuoteDetail from './pages/QuoteDetail';
 import Layout from './components/layout/Layout';
+import NotFound from './pages/NotFound';
 
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
