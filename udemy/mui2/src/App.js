4b1ce02b878e07fa59efa1e17e9f93726d42d33d
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-26 15:07:05 -0500

    add buttons

--- udemy/mui2/src/App.js
+++ udemy/mui2/src/App.js
@@ -1,8 +1,32 @@
+import { Button } from '@mui/material';
+import CarRentalIcon from '@mui/icons-material/CarRental';
+import CardTravelIcon from '@mui/icons-material/CardTravel';
+import CurrencyYuanIcon from '@mui/icons-material/CurrencyYuan';
 
 function App() {
   return (
-    <div className="App">
-      Hello, World!
+    <div>
+     <Button
+        startIcon={<CardTravelIcon/>}
+        variant="text"
+        color="secondary"
+      >
+        Text
+    </Button>
+     <Button
+        startIcon={<CarRentalIcon/>}
+        variant="contained"
+        color="success"
+     >
+         Contained
+      </Button>
+     <Button
+        startIcon={<CurrencyYuanIcon/>}
+        variant="outlined"
+        color="warning"
+       >
+        Outlined
+      </Button>
     </div>
   );
 }
