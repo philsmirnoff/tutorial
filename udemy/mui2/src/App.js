1f1b7f42d644510d6ab0476f8263093b26d83ebe
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-28 00:41:33 -0500

    adding theme

--- udemy/mui2/src/App.js
+++ udemy/mui2/src/App.js
@@ -1,9 +1,20 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, styled } from '@mui/material';
 import CarRentalIcon from '@mui/icons-material/CarRental';
 import CardTravelIcon from '@mui/icons-material/CardTravel';
 import CurrencyYuanIcon from '@mui/icons-material/CurrencyYuan';
 
 function App() {
+  const BlueButton = styled(Button)(({theme}) => ({
+    backgroundColor: 'theme.palette.otherColor.main',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: 'yellow',
+    },
+    "&:disabled": {
+      backgroundColor: 'red',
+    },
+  }));
+
   return (
     <div>
      <Button
@@ -16,20 +27,35 @@ function App() {
      <Button
         startIcon={<CarRentalIcon/>}
         variant="contained"
-        color="success"
+        color="otherColor"
      >
          Contained
       </Button>
      <Button
         startIcon={<CurrencyYuanIcon/>}
         variant="outlined"
-        color="warning"
+        color="primary"
        >
         Outlined
       </Button>
       <Typography variant="h5">
         Welcome to MUI
       </Typography>
+      <Button variant="contained" sx={{
+        backgroundColor: 'green',
+        color: 'white',
+        '&:hover': {
+          backgroundColor: 'yellow',
+        },
+        "&:disabled": {
+          backgroundColor: 'red',
+        },
+      }}>
+        Unique Button
+      </Button>
+      <BlueButton variant="contained">
+        Unique Button
+      </BlueButton>
     </div>
   );
 }
---
+++ udemy/mui2/src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    otherColor: {
+      main: '#19857b',
+    }
+  },
+});
