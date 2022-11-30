a31415dc3cf3ff61fb3da496291a09aea5c8fc9c
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-11-29 21:17:59 -0500

    add the set up for the project

--- udemy/mui2/src/App.js
+++ udemy/mui2/src/App.js
@@ -1,61 +1,15 @@
-import { Button, Typography, styled } from '@mui/material';
-import CarRentalIcon from '@mui/icons-material/CarRental';
-import CardTravelIcon from '@mui/icons-material/CardTravel';
-import CurrencyYuanIcon from '@mui/icons-material/CurrencyYuan';
+import Feed from "./components/Feed";
+import Sidebar from "./components/Sidebar";
+import Rightbar from "./components/Rightbar";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const BlueButton = styled(Button)(({theme}) => ({
-    backgroundColor: 'theme.palette.otherColor.main',
-    color: 'white',
-    '&:hover': {
-      backgroundColor: 'yellow',
-    },
-    "&:disabled": {
-      backgroundColor: 'red',
-    },
-  }));
 
   return (
     <div>
-     <Button
-        startIcon={<CardTravelIcon/>}
-        variant="text"
-        color="secondary"
-      >
-        Text
-    </Button>
-     <Button
-        startIcon={<CarRentalIcon/>}
-        variant="contained"
-        color="otherColor"
-     >
-         Contained
-      </Button>
-     <Button
-        startIcon={<CurrencyYuanIcon/>}
-        variant="outlined"
-        color="primary"
-       >
-        Outlined
-      </Button>
-      <Typography variant="h5">
-        Welcome to MUI
-      </Typography>
-      <Button variant="contained" sx={{
-        backgroundColor: 'green',
-        color: 'white',
-        '&:hover': {
-          backgroundColor: 'yellow',
-        },
-        "&:disabled": {
-          backgroundColor: 'red',
-        },
-      }}>
-        Unique Button
-      </Button>
-      <BlueButton variant="contained">
-        Unique Button
-      </BlueButton>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
     </div>
   );
 }
