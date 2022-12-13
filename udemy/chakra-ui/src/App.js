4d8829190525a210a03db31df12bad2a4da9763b
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-12-13 14:09:40 -0500

    added heading

---
+++ udemy/chakra-ui/src/App.js
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading>To Do Application</Heading>
+    </div>
+  );
+}
+
+export default App;
