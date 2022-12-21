195430fd149aa590fc5a015149bb34eef8f14f2e
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-12-20 23:18:47 -0500

    added removing todo feature

--- udemy/typescript-starting-project 8/typescript-starting-project/src/App.tsx
+++ udemy/typescript-starting-project 8/typescript-starting-project/src/App.tsx
@@ -14,12 +14,21 @@ function App() {
       return prevTodos.concat(newTodo);
     });
   }
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    })
+  }
+
+
   return (
     <div className="App">
       <NewTodo onAddTodo={addTodoHandler} />
-     <Todos items={todos}/>
+     <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
     </div>
   );
 }
+
 
 export default App;
--- udemy/typescript-starting-project 8/typescript-starting-project/src/components/TodoItem.tsx
+++ udemy/typescript-starting-project 8/typescript-starting-project/src/components/TodoItem.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Todo from '../models/todo';
 import classes from './TodoItem.module.css';
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
+const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (props) => {
   return (
-    <li className={classes.item}>{props.text}</li>
+    <li className={classes.item} onClick={props.onRemoveTodo}>{props.text}</li>
   )
 }
 
