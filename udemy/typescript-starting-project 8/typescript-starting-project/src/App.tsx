f2a08a71c6b351141e2520e37a453986d26c97bd
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-12-21 00:56:37 -0500

    aaded context api

--- udemy/typescript-starting-project 8/typescript-starting-project/src/App.tsx
+++ udemy/typescript-starting-project 8/typescript-starting-project/src/App.tsx
@@ -1,32 +1,34 @@
-import Todos from './components/Todos';
-import Todo from './models/todo';
-import NewTodo from './components/NewTodo';
-import { useState } from 'react';
-
 import './App.css';
+import TodosContextProvider from './store/todos-context';
+import Todos from './components/Todos';
+import NewTodo from './components/NewTodo';
 
 function App() {
   // const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  }
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  // const addTodoHandler = (todoText: string) => {
+  //   const newTodo = new Todo(todoText);
+  //   setTodos((prevTodos) => {
+  //     return prevTodos.concat(newTodo);
+  //   });
+  // }
 
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.id !== todoId);
-    })
-  }
+  // const removeTodoHandler = (todoId: string) => {
+  //   setTodos((prevTodos) => {
+  //     return prevTodos.filter(todo => todo.id !== todoId);
+  //   })
+  // }
 
 
   return (
-    <div className="App">
-      <NewTodo onAddTodo={addTodoHandler} />
-     <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
-    </div>
+    // <div className="App">
+    //   <NewTodo onAddTodo={addTodoHandler} />
+    //  <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
+    // </div
+    <TodosContextProvider>
+      <NewTodo />
+     <Todos />
+    </TodosContextProvider>
   );
 }
 
--- udemy/typescript-starting-project 8/typescript-starting-project/src/components/NewTodo.tsx
+++ udemy/typescript-starting-project 8/typescript-starting-project/src/components/NewTodo.tsx
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import classes from './NewTodo.module.css';
+import { TodosContext } from '../store/todos-context';
 
+const NewTodo: React.FC = () => {
 
-const NewTodo: React.FC<{onAddTodo: (text: string ) => void }> = (props) => {
+  const todosCtx = useContext(TodosContext);
   const todoTextInputRef = useRef<HTMLInputElement>(null)
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault()
@@ -12,7 +14,8 @@ const NewTodo: React.FC<{onAddTodo: (text: string ) => void }> = (props) => {
       // throw an error
       return;
     }
-    props.onAddTodo(enteredText)
+    // props.onAddTodo(enteredText)
+    todosCtx.addTodo(enteredText);
   }
   return (
     <form onSubmit={submitHandler} className={classes.form}>
