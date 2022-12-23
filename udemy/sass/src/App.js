3b37962c4942896aba505744c7d43b0c47a374c4
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-12-23 16:27:18 -0500

    added variables

---
+++ udemy/sass/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+import MainPage from './components/MainPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ udemy/sass/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/MainPage.css';
+
+const MainPage = () => {
+  return (
+    <div className="mainPage">
+      <h1>Wellcome to our Website</h1>
+      </div>
+  )
+}
+
+export default MainPage
