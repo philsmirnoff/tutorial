7af124df71e0f854d27b864bff7c6eceb022081a
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-08-06 00:15:31 -0400

    initial set up

---
+++ ai-trip-planner/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h2>Wellcome to AI Trip Planner</h2>
+      <p>AI Trip Planner is a web application that helps you plan your trip.</p>
+      <Button variant="default">Get Started</Button>
+    </>
+  )
+}
+
+export default App
